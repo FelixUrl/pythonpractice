fb2cc11e70e0fd2813132c16fbb6669e8c2452bb
sShep000 <roman.sharifullin.2002@mail.ru>
2021-05-13 18:52:22 +0500

    2

--- js/index.js
+++ js/index.js
@@ -6,15 +6,3 @@ function openmenu(e) {
     menu.classList.toggle('active');
     main.classList.toggle('active')
 }
-function ShowCategory(e){
-    let spoiler = document.getElementById('spoiler');
-    let arrow = document.getElementById('arrow');
-    let list = document.getElementById('list');
-    list.classList.toggle('one');
-    if(list.classList.contains('one')){
-        spoiler.classList.remove('active');
-        arrow.classList.remove('active');
-    }
-    spoiler.classList.toggle('active');
-    arrow.classList.toggle('active');
-}
