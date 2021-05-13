40fceeb7095f93f51bade8fa78d645c06c9f581f
sShep000 <55055955+sShep000@users.noreply.github.com>
2021-05-13 18:54:51 +0500

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-function openmenu(e) {
-    e.classList.toggle('active');
-    let menu = document.getElementById("menu");
-    let main = document.getElementById("content_body");
-    menu.classList.toggle('active');
-    main.classList.toggle('active')
-}
